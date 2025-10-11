e3b80c5ebe192e29569084a41c00cab5ac77fe99
FajarVerse <gifarifajarm174@gmail.com>
2025-10-11 10:06:02 +0700

    fix: modify project-section

--- src/fragment/projects/project-fragment.tsx
+++ src/fragment/projects/project-fragment.tsx
@@ -3,35 +3,49 @@ import Header from "@/components/main/header";
 import AuthLayout from "@/layout/auth-layout";
 import projectTest from "../../../public/image/project-test.png";
 
+const projects = [
+  {
+    id: 1,
+    title: "Lorem Ipsum dolor",
+    stacks: ["next js", "laravel"],
+    liveUrl: "",
+    repoUrl: "",
+    status: true,
+    image: projectTest.src,
+  },
+  {
+    id: 2,
+    title: "Lorem Ipsum dolor",
+    stacks: ["next js", "laravel"],
+    liveUrl: "",
+    repoUrl: "",
+    status: true,
+    image: projectTest.src,
+  },
+];
+
 const ProjectFragment = () => {
   return (
     <>
       <AuthLayout className="pt-32 lg:pb-28" id="projects">
         <Header>My Projects</Header>
         <div className="w-full mt-5 grid grid-cols-1 gap-10 md:gap-12 md:mt-8 lg:mt-10">
-          {/* <CardProject>
-            <CardProject.Header image={projectTest.src} />
-            <CardProject.Body title="Lugi Store">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-              natus temporibus earum architecto ipsa! Sit nesciunt culpa
-              adipisci odit et.
-            </CardProject.Body>
-            <CardProject.Footer liveUrl="" repoUrl="" status={true} />
-          </CardProject> */}
-          <CardProject>
-            <CardProject.LeftContent image={projectTest.src} />
-            <CardProject.RightContent
-              title="Lorem Ipsum dolor"
-              stacks={["next js", "laravel"]}
-              liveUrl=""
-              repoUrl=""
-              status={true}
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-              minus illo, doloribus animi ducimus exercitationem culpa veniam
-              beatae perferendis officia!
-            </CardProject.RightContent>
-          </CardProject>
+          {projects.map((project) => (
+            <CardProject key={project.id}>
+              <CardProject.LeftContent image={project.image} />
+              <CardProject.RightContent
+                title={project.title}
+                stacks={project.stacks}
+                liveUrl={project.liveUrl}
+                repoUrl={project.repoUrl}
+                status={project.status}
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                minus illo, doloribus animi ducimus exercitationem culpa veniam
+                beatae perferendis officia!
+              </CardProject.RightContent>
+            </CardProject>
+          ))}
         </div>
       </AuthLayout>
     </>
