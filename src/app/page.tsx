0267442d334c059eaef1529e89bab3d3ca693efd
FajarVerse <gifarifajarm174@gmail.com>
2025-10-13 21:10:04 +0700

    feat: create new cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
+import { SmoothCursor } from "@/components/ui/smooth-cursor";
 import Footer from "@/layout/footer";
 import Navbar from "@/layout/navbar";
 import About from "@/page/about";
@@ -20,6 +21,7 @@ export default function Page() {
       <Contact />
       <Footer />
       <AnimatedThemeToggler className="fixed bottom-10 right-5 z-40 rounded-full p-2 bg-background shadow-[0_0px_6px_0.5px_rgba(0,0,0,0.3)] dark:shadow-[0_0_10px_3px_rgba(255,255,255,0.2)] md:bottom-14 md:right-10 lg:hidden" />
+      <SmoothCursor />
     </>
   );
 }
