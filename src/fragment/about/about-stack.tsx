153577909b77b8194424e658191ff92f9fa73b5f
FajarVerse <gifarifajarm174@gmail.com>
2025-12-27 13:16:09 +0700

    fix: update next js

--- src/fragment/about/about-stack.tsx
+++ src/fragment/about/about-stack.tsx
@@ -83,8 +83,8 @@ const AboutStack = () => {
                   />
                 ))}
             </Marquee>
-            <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r"></div>
-            <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l"></div>
+            <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-linear-to-r"></div>
+            <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-linear-to-l"></div>
           </div>
         </div>
       </BlurFade>
